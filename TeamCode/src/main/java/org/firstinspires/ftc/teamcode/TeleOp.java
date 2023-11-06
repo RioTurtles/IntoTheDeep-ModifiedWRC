8a4b9eca1e3534f6ef40ad5ccda78ff48b5f95c1
Airblox <106415728+Airblox@users.noreply.github.com>
2023-11-06 22:19:09 +0800

    Re-add overwritten code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -10,6 +10,9 @@ public class TeleOp extends LinearOpMode {
         Hardware robot = new Hardware();
         robot.init(hardwareMap);
 
+        // Variables
+        int sliderHeight = 0;
+
         telemetry.addData("Status", "Initialized");  // Robot finishes initialization. Here we output a debug message.
         telemetry.update();
         robot.reset();  // Reset robot?
@@ -32,17 +35,72 @@ public class TeleOp extends LinearOpMode {
             // The above 2 have been reversed.
             robot.motorBackRight.setPower(-(lx + ly - rot_x) / denominator);
 
-            if (gamepad1.options) {
+            if (gamepad1.options) {  // Reset IMU
                 robot.resetIMU();
             }
 
-            if (gamepad1.circle) {
-                robot.servoTest.setPower(robot.servoTest.getPower() + 50);
-                telemetry.addData("servoPower", robot.servoTest.getPower());
+            if (gamepad1.right_bumper) {
+                // Control arm with right stick and intake pitch (vertical) with left stick when R1
+                robot.servoArmRight.setPosition(robot.servoArmRight.getPosition() + right_y / 10);
+                robot.servoArmLeft.setPosition(robot.servoArmLeft.getPosition() + right_y / 10);
+                robot.servoIntakePitchLeft.setPosition(robot.servoIntakePitchLeft.getPosition() + left_y / 10);
+                robot.servoIntakePitchRight.setPosition(robot.servoIntakePitchRight.getPosition() + left_y / 10);
+            } else {
+                robot.motorFrontLeft.setPower((lx + ly + rot_x) / denominator);
+                robot.motorBackLeft.setPower((-lx + ly + rot_x) / denominator);
+                robot.motorFrontRight.setPower((ly - lx - rot_x) / denominator);
+                robot.motorBackRight.setPower((lx + ly - rot_x) / denominator);
             }
-            if (gamepad1.square) {
-                robot.servoTest.setPower(robot.servoTest.getPower() - 50);
-                telemetry.addData("servoPower", robot.servoTest.getPower());
+
+            if (gamepad1.right_trigger >= Constants.rightTriggerFullyDown) {
+                // R2, open intake
+                robot.servoIntakeLeft.setPower(-gamepad1.right_trigger);
+                robot.servoIntakeRight.setPower(gamepad1.right_trigger);
+            } else if (gamepad1.left_trigger >= Constants.leftTriggerFullyDown) {
+                // L2, close intake
+                robot.servoIntakeLeft.setPower(gamepad1.left_trigger);
+                robot.servoIntakeRight.setPower(-gamepad1.left_trigger);
+            }
+
+            if (gamepad1.dpad_left) {
+                // dpad left, extend intake
+                robot.servoArmLeft.setPosition(Constants.armLeftExtended);
+                robot.servoArmRight.setPosition(Constants.armRightExtended);
+            } else if (gamepad1.dpad_right) {
+                // dpad right, retract intake
+                robot.servoArmLeft.setPosition(Constants.armLeftRetracted);
+                robot.servoArmRight.setPosition(Constants.armRightRetracted);
+            }
+
+            if (gamepad1.dpad_down) {
+                // dpad down, intake position
+                if (sliderHeight == 2) {
+                    robot.motorHeightLeft.setTargetPosition(Constants.heightLeftHigh);
+                    robot.motorHeightRight.setTargetPosition(Constants.heightRightHigh);
+                    robot.servoIntakePitchLeft.setPosition(Constants.intakePitchLeftGrab);
+                    robot.servoIntakePitchRight.setPosition(Constants.intakePitchRightGrab);
+                    sliderHeight = 1; }
+            } else if (gamepad1.dpad_up) {
+                // dpad up, scoring position
+                if (sliderHeight == 1) {
+                    robot.motorHeightLeft.setTargetPosition(Constants.heightLeftDown);
+                    robot.motorHeightRight.setTargetPosition(Constants.heightRightDown);
+                    robot.servoIntakePitchLeft.setPosition(Constants.intakePitchLeftScore);
+                    robot.servoIntakePitchRight.setPosition(Constants.intakePitchRightScore);
+                    sliderHeight = 2; }
+            } else if (gamepad1.square) {
+                // square, max height
+                robot.motorHeightLeft.setTargetPosition(Constants.maxLeftHeight);
+                robot.motorHeightRight.setTargetPosition(Constants.maxRightHeight);
+            } else if (gamepad1.circle) {
+                // circle, min height
+                robot.motorHeightLeft.setTargetPosition(Constants.minLeftHeight);
+                robot.motorHeightRight.setTargetPosition(Constants.minRightHeight);
+            }
+
+            if (gamepad1.triangle) {
+                // launch drone
+                robot.servoDroneLauncher.setPosition(Constants.droneRelease);
             }
 
             telemetry.update();
