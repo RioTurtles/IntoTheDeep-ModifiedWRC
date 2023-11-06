cb15e1a675a1bbdf0ddf024f0f146cb966bd4bdd
Airblox <106415728+Airblox@users.noreply.github.com>
2023-11-06 22:11:13 +0800

    Fieldcentric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,53 +1,56 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 public class Hardware {
     DcMotor motorFrontLeft;
     DcMotor motorFrontRight;
     DcMotor motorBackLeft;
     DcMotor motorBackRight;
-    DcMotor motorHeightLeft;
-    DcMotor motorHeightRight;
-    Servo servoArmLeft;
-    Servo servoArmRight;
-    Servo servoIntakePitchLeft;
-    Servo servoIntakePitchRight;
-    CRServo servoIntakeLeft;
-    CRServo servoIntakeRight;
+    DcMotor motorArmLeft;
+    DcMotor motorArmRight;
+    DcMotor motorRiggingLinearActuatorLeft;
+    DcMotor motorRiggingLinearActuatorRight;
+    CRServo servoTest;
+    Servo servoIntakeLeft;
+    Servo servoIntakeRight;
     Servo servoDroneLauncher;
+    DistanceSensor sensorDistance;
     IMU imu;
 
-    public Hardware(HardwareMap hardwareMap) {
+    public void init(HardwareMap hardwareMap) {
         this.motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft");
         this.motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight");
         this.motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft");
         this.motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight");
-        this.motorHeightLeft = hardwareMap.get(DcMotor.class, "motorHeightLeft");
-        this.motorHeightRight = hardwareMap.get(DcMotor.class, "motorHeightRight");
-        this.servoArmLeft = hardwareMap.get(Servo.class, "servoArmLeft");
-        this.servoArmRight = hardwareMap.get(Servo.class, "servoArmRight");
-        this.servoIntakePitchLeft = hardwareMap.get(Servo.class, "servoIntakePitchLeft");
-        this.servoIntakePitchRight = hardwareMap.get(Servo.class, "servoIntakePitchRight");
-        this.servoIntakeLeft = hardwareMap.get(CRServo.class, "servoIntakeLeft");
-        this.servoIntakeRight = hardwareMap.get(CRServo.class, "servoIntakeRight");
+        this.servoTest = hardwareMap.get(CRServo.class, "motorTest");
+        /*
+        this.motorArmLeft = hardwareMap.get(DcMotor.class, "motorArmLeft");
+        this.motorArmRight = hardwareMap.get(DcMotor.class, "motorArmRight");
+        this.motorRiggingLinearActuatorLeft = hardwareMap.get(DcMotor.class, "motorRiggingLinearActuatorLeft");
+        this.motorRiggingLinearActuatorRight = hardwareMap.get(DcMotor.class, "motorRiggingLinearActuatorRight");
+        this.servoIntakeLeft = hardwareMap.get(Servo.class, "servoIntakeLeft");
+        this.servoIntakeRight = hardwareMap.get(Servo.class, "servoIntakeRight");
         this.servoDroneLauncher = hardwareMap.get(Servo.class, "servoDroneLauncher");
+        this.sensorDistance = hardwareMap.get(DistanceSensor.class, "sensorDistance");
+         */
         this.imu = hardwareMap.get(IMU.class, "imu");
     }
 
     public void reset() {
         this.motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         this.motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        this.servoArmLeft.setPosition(Constants.armLeftRetracted);
-        this.servoArmRight.setPosition(Constants.armRightRetracted);
         this.imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)));
     }
 
-
+    public void resetIMU() {
+        this.imu.resetYaw();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -3,100 +3,49 @@ package org.firstinspires.ftc.teamcode;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="v1 TeleOp")
 public class TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Hardware robot = new Hardware(hardwareMap);
+        Hardware robot = new Hardware();
+        robot.init(hardwareMap);
 
-        // variables
-        int sliderHeight = 1;
-
-        telemetry.addData("Status", "Initialized");
-        robot.reset();
+        telemetry.addData("Status", "Initialized");  // Robot finishes initialization. Here we output a debug message.
+        telemetry.update();
+        robot.reset();  // Reset robot?
         waitForStart();
 
         while (opModeIsActive()) {
+            // Fieldcentric
             double bot_heading = robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             double left_x = gamepad1.left_stick_x;
             double left_y = -gamepad1.left_stick_y;
-            double right_y = gamepad1.right_stick_y;
-            double rot_x = gamepad1.right_stick_x;
+            double rot_x = gamepad1.right_stick_x*0.6;  // Make rotation less intense (60%)
             double lx = left_x * Math.cos(-bot_heading) - left_y * Math.sin(-bot_heading);
             double ly = left_x * Math.sin(-bot_heading) + left_y * Math.cos(-bot_heading);
             double denominator = Math.max(Math.abs(left_x) + Math.abs(left_y) + Math.abs(rot_x), 1);
 
-            // don't know if this will work (need tuning)
-            if (gamepad1.right_bumper) {
-                // control arm with right stick and intake pitch (vertical) with left stick when R1
-                robot.servoArmRight.setPosition(robot.servoArmRight.getPosition() + right_y / 10);
-                robot.servoArmLeft.setPosition(robot.servoArmLeft.getPosition() + right_y / 10);
-                robot.servoIntakePitchLeft.setPosition(robot.servoIntakePitchLeft.getPosition() + left_y / 10);
-                robot.servoIntakePitchRight.setPosition(robot.servoIntakePitchRight.getPosition() + left_y / 10);
-            } else {
-                robot.motorFrontLeft.setPower((lx + ly + rot_x) / denominator);
-                robot.motorBackLeft.setPower((-lx + ly + rot_x) / denominator);
-                robot.motorFrontRight.setPower((ly - lx - rot_x) / denominator);
-                robot.motorBackRight.setPower((lx + ly - rot_x) / denominator);
-            }
+            robot.motorFrontLeft.setPower(-(lx + ly + rot_x) / denominator);
+            // The below 2 have been reversed.
+            robot.motorBackLeft.setPower(-(-lx + ly + rot_x) / denominator);
+            robot.motorFrontRight.setPower(-(ly - lx - rot_x) / denominator);
+            // The above 2 have been reversed.
+            robot.motorBackRight.setPower(-(lx + ly - rot_x) / denominator);
 
             if (gamepad1.options) {
-                // Options, reset IMU yaw
-                robot.imu.resetYaw();
+                robot.resetIMU();
             }
 
-            if (gamepad1.right_trigger >= Constants.rightTriggerFullyDown) {
-                // R2, open intake
-                robot.servoIntakeLeft.setPower(-gamepad1.right_trigger);
-                robot.servoIntakeRight.setPower(gamepad1.right_trigger);
-            } else if (gamepad1.left_trigger >= Constants.leftTriggerFullyDown) {
-                // L2, close intake
-                robot.servoIntakeLeft.setPower(gamepad1.left_trigger);
-                robot.servoIntakeRight.setPower(-gamepad1.left_trigger);
+            if (gamepad1.circle) {
+                robot.servoTest.setPower(robot.servoTest.getPower() + 50);
+                telemetry.addData("servoPower", robot.servoTest.getPower());
+            }
+            if (gamepad1.square) {
+                robot.servoTest.setPower(robot.servoTest.getPower() - 50);
+                telemetry.addData("servoPower", robot.servoTest.getPower());
             }
 
-            if (gamepad1.dpad_left) {
-                // dpad left, extend intake
-                robot.servoArmLeft.setPosition(Constants.armLeftExtended);
-                robot.servoArmRight.setPosition(Constants.armRightExtended);
-            } else if (gamepad1.dpad_right) {
-                // dpad right, retract intake
-                robot.servoArmLeft.setPosition(Constants.armLeftRetracted);
-                robot.servoArmRight.setPosition(Constants.armRightRetracted);
-            }
-
-            if (gamepad1.dpad_down) {
-                // dpad down, intake position
-                if (sliderHeight == 2) {
-                robot.motorHeightLeft.setTargetPosition(Constants.heightLeftHigh);
-                robot.motorHeightRight.setTargetPosition(Constants.heightRightHigh);
-                robot.servoIntakePitchLeft.setPosition(Constants.intakePitchLeftGrab);
-                robot.servoIntakePitchRight.setPosition(Constants.intakePitchRightGrab);
-                sliderHeight = 1; }
-            } else if (gamepad1.dpad_up) {
-                // dpad up, scoring position
-                if (sliderHeight == 1) {
-                robot.motorHeightLeft.setTargetPosition(Constants.heightLeftDown);
-                robot.motorHeightRight.setTargetPosition(Constants.heightRightDown);
-                robot.servoIntakePitchLeft.setPosition(Constants.intakePitchLeftScore);
-                robot.servoIntakePitchRight.setPosition(Constants.intakePitchRightScore);
-                sliderHeight = 2; }
-            } else if (gamepad1.square) {
-                // square, max height
-                robot.motorHeightLeft.setTargetPosition(Constants.maxLeftHeight);
-                robot.motorHeightRight.setTargetPosition(Constants.maxRightHeight);
-            } else if (gamepad1.circle) {
-                // circle, min height
-                robot.motorHeightLeft.setTargetPosition(Constants.minLeftHeight);
-                robot.motorHeightRight.setTargetPosition(Constants.minRightHeight);
-            }
-
-            if (gamepad1.triangle) {
-                // launch drone
-                robot.servoDroneLauncher.setPosition(Constants.droneRelease);
-            }
-
-
-            }
+            telemetry.update();
         }
     }
+}
